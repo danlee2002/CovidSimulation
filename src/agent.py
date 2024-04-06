39a019b69b8d3f80dfafcd526520e354ad812b12
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-04-05 22:30:59 -0400

    updated model

--- src/agent.py
+++ src/agent.py
@@ -1,21 +1,19 @@
 import numpy as np
 # notes Well-Riley model/need to add accomodation for airflow and consumption 
-def simulate(I, q, p,t, V, p_vaccine):
+def simulate(I, q, p,t, V, p_mask):
     ventilation_rate = 3 
-    social_distancing_effectiveness = 0.8 
+    social_distancing_effectiveness = 0.1
     effective_volume = V * ventilation_rate
-    P_unadjusted = 1 - np.exp(-I * q * p * t / effective_volume)
-    P_adjusted = P_unadjusted * (1 - social_distancing_effectiveness) * (1 -p_vaccine)
+    P_unadjusted = 1 - np.exp(-I * (q * 0.28 *(1- p_mask)) * p * t / effective_volume)
     print(f"Unadjusted Probability of infection: {P_unadjusted*100:.2f}%")
-    print(f"Adjusted Probability of infection with social distancing: {P_adjusted*100:.2f}%")
 
 def main():
     I = 1  
-    q = 10 
+    q = 100 
     p = 0.32  
-    t = 8  
-    V = 60  
-    simulate(I,q,p,t,V,0.2)
+    V = 45 
+    simulate(I,q,p,7.52,V,0.94)
+    simulate(I,q,p,4.93,V,0.09)
 
 if __name__ == '__main__':
     main()
